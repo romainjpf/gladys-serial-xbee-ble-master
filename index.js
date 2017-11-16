5e70da0155537102fea1cd9252f8fa5f1a095867
romainjpf <romainjpf.forestier@gmail.com>
2017-11-16 19:11:55 +0100

    add uninstall function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function(sails) {
     var connect = require('./lib/connect.js');
     var sendCode = require('./lib/sendCode.js');
     var install = require('./lib/install.js');
+    var install = require('./lib/uninstall.js');
 
     gladys.on('ready', function(){
         connect();
@@ -11,6 +12,7 @@ module.exports = function(sails) {
 
     return {
         install: install,
+        uninstall: uninstall,
         connect: connect,
         setup: setup,
         sendCode: sendCode
